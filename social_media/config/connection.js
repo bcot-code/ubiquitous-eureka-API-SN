62ce300302d08be331e216ee64171d27f8b57eb4
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-31 00:54:38 -0600

    creating pathway

--- social_media/config/connection.js
+++ social_media/config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require("mongoose");
 
-const connectionUrl = "mongodb://127.0.0.1:27017";
+const connectionUrl = "mongodb://127.0.0.1:27017/eurekaApi";
 connect(connectionUrl);
 module.exports = connection;
