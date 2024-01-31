19d678127273e2edca36ab689516cdc4558d2b20
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-31 01:00:42 -0600

    initialize user db

--- social_media/controllers/user.js
+++ social_media/controllers/user.js
@@ -1,1 +1,9 @@
-const{} = require('../models');
+const { User } = require("../models");
+
+module.exports = {
+  // get all users
+  // get single uer by id and populated thought and friend data
+  // POST create a new user
+  // PUT to update a user by id
+  // DELETE to remove user by id
+};
