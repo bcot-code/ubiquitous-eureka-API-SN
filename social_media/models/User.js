0fb853b479375f5889e28d94f7598977e1b6de75
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-01-31 00:05:10 -0600

    fixing User

--- social_media/models/User.js
+++ social_media/models/User.js
@@ -1,39 +1,49 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true,
-    trimmed,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: "Email validation failed",
+      },
+    },
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: () => Promise.resolve(false),
-      message: "Email validation failed",
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
     },
-  },
-  thoughts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Thought",
-    },
-  ],
-  friends: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-});
+    id: false,
+  }
+);
 
 //Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
-userSchema.virtual("friendCount").get(function() {
+userSchema.virtual("friendCount").get(function () {
   return this.friends.length;
 });
 
-const User = mongoose.model("User", userSchema);
+const User = model("User", userSchema);
 module.exports = User;
