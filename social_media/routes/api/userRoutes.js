6bffb148f27f4532c436fcd9ce2bc9d3eeb71330
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-02-01 22:33:37 -0600

    add,create,delete User and Friend

--- social_media/routes/api/userRoutes.js
+++ social_media/routes/api/userRoutes.js
@@ -5,11 +5,19 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend,
 } = require("../controllers/users_controller");
 
 // /api/users
 router.route("/").get(getUsers).post(createUser);
 // /api/users/:userId
 router.route("/:userId").get(getSingleUser).delete(deleteUser).put(updateUser);
+//add friend to user's friends list
+router
+  .route("/:userId/friends/:friendId")
+  .post(addFriend)
+  //remove a friend from the users friend list
+  .delete(removeFriend);
 
 module.exports = router;
