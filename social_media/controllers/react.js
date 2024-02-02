892a84ff4fcdb774c277568edd9ddac0678c6993
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-02-01 22:29:20 -0600

    delete and added to thought.js

--- social_media/controllers/react.js
+++
@@ -1,7 +0,0 @@
-const { React } = require("../models/Reaction");
-
-module.exports = {
-  //POST to create a reaction stored in a single thought's reactions array field
-  async,
-  //DELETE to pull and remove a reaction by the reaction's reactionId value
-};
