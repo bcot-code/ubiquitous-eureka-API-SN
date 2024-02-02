91310e27e8f44bea84f7c06abfb20ec787d56559
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-02-01 21:21:10 -0600

    adding thought

---
+++ social_media/controllers/thought.js
@@ -0,0 +1,9 @@
+const { Thought } = require("../models/Thought");
+
+module.exports = {
+  //Get all thoughts
+  //Get a single thought by its `id` parameter. Returns a JSON object of the thought data values.
+  //POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+  //PUT to update a thought by its _id
+  //DELETE to remove a thought by its _id
+};
