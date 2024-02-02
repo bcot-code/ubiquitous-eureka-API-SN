25960f5750f1dd41027807d327ecb9e6dcbf861b
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-02-01 22:30:09 -0600

    adding the reaction to thought

--- social_media/controllers/thought.js
+++ social_media/controllers/thought.js
@@ -1,9 +1,14 @@
-const { Thought } = require("../models/Thought");
+const { Thought, User } = require("../models");
 
 module.exports = {
+  // /api/thoughts
   //Get all thoughts
   //Get a single thought by its `id` parameter. Returns a JSON object of the thought data values.
   //POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
   //PUT to update a thought by its _id
   //DELETE to remove a thought by its _id
+  // /api/thoughts/:thoughtId/reactions
+  //POST to create a reaction stored in a single thought's reactions array field
+  async,
+  //DELETE to pull and remove a reaction by the reaction's reactionId value
 };
