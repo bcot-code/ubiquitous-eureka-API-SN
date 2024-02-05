69cb3cfe72100a029428fbabf79ea0ab9e5f6b90
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-02-05 03:11:28 -0600

    changing the title

--- social_network/config/connection.js
+++ social_network/config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require("mongoose");
 
-const connectionUrl = "mongodb://127.0.0.1:27017/eurekaApi";
+const connectionUrl = "mongodb://127.0.0.1:27017/socialmediaApi";
 connect(connectionUrl);
 module.exports = connection;
