2b996353b64207d66133d1846fee571d0f3f0c4d
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-02-05 03:29:49 -0600

    correcting pathway,fixing add friend and removing

--- social_network/controllers/user.js
+++ social_network/controllers/user.js
@@ -1,52 +1,61 @@
-const { User, Thought } = require("../models/User");
+const { User, Thought } = require("../models");
 
 module.exports = {
   // /api/users
   // get all users
-  getUsers(req, res) {
-    User.find()
-      .then((users) => {
-        if (!users || users.length === 0) {
-          return res.status(404).json({ message: "No users found" });
-        }
-        res.json(users);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).send();
-      });
+  async getUsers(req, res) {
+    try {
+      const users = await User.find()
+        .populate({
+          path: "thoughts",
+          select: "-__v",
+        })
+        .populate({
+          path: "friends",
+          selec: "-__v",
+        })
+        .select("-__v")
+        .sort({ _id: -1 });
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
-  // get single uer by id and populated thought and friend data
-  getSingleUser(req, res) {
-    User.findOne({ username: req.params.username })
-      .populate({
-        path: "thoughts",
-        select: "-__v",
-      })
-      .select("-__v")
-      .then((user) => {
-        if (!user) {
-          return res
-            .status(404)
-            .json({ message: "No user with that username!" });
-        }
-        res.json(user);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
+  // get single user by id and populated thought and friend data
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId }).populate([
+        {
+          path: "thoughts",
+          select: "-__v",
+        },
+        {
+          path: "friends",
+          select: "-__v",
+        },
+      ]);
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID!" });
+      }
+      res.json({
+        user: user,
+        totalThoughts: user.thoughts.length,
+        friends: user.friends.length,
       });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   },
   // POST create a new user
-  createUser(req, res) {
-    User.create(req.body)
-      .then((user) => {
-        res.json(user);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
+  async createUser({ body }, res) {
+    try {
+      const user = await User.create(body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   },
   // PUT to update a user by id and populated thought and friend data
   updateUser(req, res) {
@@ -70,7 +79,7 @@ module.exports = {
   },
   // DELETE to remove user by id
   deleteUser(req, res) {
-    User.remove({ _id: req.params.userId })
+    User.findOneAndDelete({ _id: req.params.userId })
       .then((user) => {
         if (!user) {
           res.json({ removed: req.params.userId });
@@ -88,8 +97,8 @@ module.exports = {
   addFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $push: { friends: req.params.friendId } },
-      { new: true }
+      { $addToSet: { friends: req.params.friendId } },
+      { new: true, runValidators: true }
     )
       .then((users) => {
         if (!users) {
@@ -97,7 +106,7 @@ module.exports = {
             .status(404)
             .json({ message: "No user found with this id!" });
         }
-        res.json(user);
+        res.json(users);
       })
       .catch((err) => {
         console.log(err);
